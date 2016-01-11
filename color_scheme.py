aa91e8c563f477c79c87b132d6b044088a4a274e
rkp8000 <rpang.contact@gmail.com>
2016-01-11 12:41:56 -0800

    Remove _.

--- color_scheme.py
+++ color_scheme.py
@@ -11,4 +11,4 @@ TARGETATTRACTION = np.array([221, 204, 119]) / 255.
 CONFIG = np.array([153, 153, 51]) / 255.
 ATLAS = np.array([170, 68, 153]) / 255.
 GEOMDIRECTED = np.array([0, 0, 0]) / 255.
-SG_TOPO = np.array([136, 204, 238]) / 255.
+SGTOPO = np.array([136, 204, 238]) / 255.
