2eaba6693717cb49b0e3ee7769ef268e664ca70d
rkp8000 <rpang.contact@gmail.com>
2016-01-11 13:06:42 -0800

    Make GEOMDIRECTED gray, not black.

--- color_scheme.py
+++ color_scheme.py
@@ -10,5 +10,5 @@ PREFATTACHMENT = np.array([221, 204, 119]) / 255.
 TARGETATTRACTION = np.array([221, 204, 119]) / 255.
 CONFIG = np.array([153, 153, 51]) / 255.
 ATLAS = np.array([170, 68, 153]) / 255.
-GEOMDIRECTED = np.array([0, 0, 0]) / 255.
+GEOMDIRECTED = np.array([.5, .5, .5]) / 255.
 SGTOPO = np.array([136, 204, 238]) / 255.
